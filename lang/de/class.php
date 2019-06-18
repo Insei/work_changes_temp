26a50c8e1a5f92d1562c046f98fe4dfc81372391
Insei <goodmobiledevices@gmail.com>
2019-06-18 15:42:07 +0300

    current

---
+++ lang/de/class.php
@@ -0,0 +1,86 @@
+<?
+$MESS["CRM_DEAL_FIELD_RECURRING_MUTLTIPLE_PERIOD_TITLE"] = "Wiederholungsintervall";
+$MESS["CRM_DEAL_FIELD_RECURRING_CUSTOM_INTERVAL"] = "Benutzerdefiniertes Intervall";
+$MESS["CRM_DEAL_FIELD_RECURRING_CUSTOM_INTERVAL_TITLE"] = "Wiederholen alle";
+$MESS["CRM_DEAL_FIELD_RECURRING_SINGLE_TITLE"] = "Auftrag erstellen";
+$MESS["CRM_DEAL_FIELD_RECURRING_MULTIPLE_FINAL_LIMIT_TITLE"] = "Wiederholung stoppen";
+$MESS["CRM_DEAL_FIELD_RECURRING_MULTIPLE_FINAL_NO_LIMIT"] = "Nie";
+$MESS["CRM_DEAL_FIELD_RECURRING_MULTIPLE_FINAL_LIMIT_DATE"] = "wenn Enddatum erreicht";
+$MESS["CRM_DEAL_FIELD_RECURRING_MULTIPLE_FINAL_LIMIT_TIMES"] = "wenn Widerholungsmaximum erreicht";
+$MESS["CRM_DEAL_FIELD_RECURRING_MULTIPLE_START_DATE_TITLE"] = "Starten am";
+$MESS["CRM_DEAL_FIELD_RECURRING_MULTIPLE_LIMIT_DATE_TITLE"] = "Beenden am";
+$MESS["CRM_DEAL_FIELD_RECURRING_MULTIPLE_LIMIT_TIMES_TITLE"] = "Wiederholungsmaximum";
+$MESS["CRM_DEAL_FIELD_RECURRING_MANY_TIMES"] = "Regelmäßig";
+$MESS["CRM_DEAL_FIELD_RECURRING_ONCE_TIME"] = "Nur einmal";
+$MESS["CRM_DEAL_FIELD_RECURRING_CUSTOM_DAY"] = "Tag(e)";
+$MESS["CRM_DEAL_FIELD_RECURRING_CUSTOM_WEEK"] = "Woche(n)";
+$MESS["CRM_DEAL_FIELD_RECURRING_CUSTOM_MONTH"] = "Monat(e)";
+$MESS["CRM_MODULE_NOT_INSTALLED"] = "Das Modul CRM ist nicht installiert.";
+$MESS["CRM_PERMISSION_DENIED"] = "Zugriff verweigert";
+$MESS["CRM_DEAL_FIELD_TITLE"] = "Name";
+$MESS["CRM_DEAL_FIELD_TYPE_ID"] = "Auftragstyp";
+$MESS["CRM_DEAL_FIELD_STAGE_ID"] = "Phase";
+$MESS["CRM_DEAL_FIELD_OPPORTUNITY_WITH_CURRENCY"] = "Betrag und Währung";
+$MESS["CRM_DEAL_FIELD_CLOSEDATE"] = "Abschlussdatum";
+$MESS["CRM_DEAL_FIELD_BEGINDATE"] = "Anfangsdatum";
+$MESS["CRM_DEAL_FIELD_PROBABILITY"] = "Wahrscheinlichkeit";
+$MESS["CRM_DEAL_FIELD_OPENED"] = "Freigegeben für alle";
+$MESS["CRM_DEAL_FIELD_COMMENTS"] = "Kommentar";
+$MESS["CRM_DEAL_FIELD_CLIENT"] = "Kunde";
+$MESS["CRM_DEAL_FIELD_ASSIGNED_BY_ID"] = "Verantwortlich";
+$MESS["CRM_DEAL_FIELD_RECURRING"] = "Wiederholung";
+$MESS["CRM_DEAL_FIELD_CATEGORY_RECURRING"] = "Auftragspipeline";
+$MESS["CRM_DEAL_FIELD_OPPORTUNITY"] = "Betrag";
+$MESS["CRM_DEAL_FIELD_CURRENCY_ID"] = "Währung";
+$MESS["CRM_DEAL_FIELD_PRODUCTS"] = "Produkte";
+$MESS["CRM_DEAL_SECTION_MAIN"] = "Zum Auftrag";
+$MESS["CRM_DEAL_SECTION_ADDITIONAL"] = "Mehr";
+$MESS["CRM_DEAL_SECTION_RECURRING"] = "Wiederkehrender Auftrag";
+$MESS["CRM_DEAL_SECTION_PRODUCTS"] = "Produkte";
+$MESS["CRM_DEAL_TAB_PRODUCTS"] = "Produkte";
+$MESS["CRM_DEAL_TAB_INVOICES"] = "Rechnungen";
+$MESS["CRM_DEAL_TAB_AUTOMATION"] = "Automatisierung";
+$MESS["CRM_DEAL_TAB_EVENT"] = "History";
+$MESS["CRM_DEAL_TAB_QUOTE"] = "Angebote";
+$MESS["CRM_DEAL_TAB_BIZPROC"] = "Workflows";
+$MESS["CRM_DEAL_TAB_TREE"] = "Abhängigkeiten";
+$MESS["CRM_DEAL_FIELD_RECURRING_NOTHING_SELECTED"] = "Nichts ausgewählt";
+$MESS["CRM_DEAL_FIELD_RECURRING_RESTRICTED"] = "Sie haben nicht genügend Rechte";
+$MESS["CRM_DEAL_FIELD_RECURRING_CREATED_FROM_CURRENT"] = "Eine Vorlage wurde für den wiederkehrenden Auftrag ##RECURRING_ID# erstellt";
+$MESS["CRM_DEAL_FIELD_RECURRING_CREATED_MANY_FROM_CURRENT"] = "Vorlagen wurden für die wiederkehrenden Aufträge #RECURRING_LIST# erstellt";
+$MESS["CRM_DEAL_FIELD_RECURRING_DATE_NEXT_EXECUTION"] = "Nächster Auftrag wird erstellt am #NEXT_DATE#";
+$MESS["CRM_DEAL_FIELD_RECURRING_TITLE"] = "Auftragsvorlage: #TITLE#";
+$MESS["CRM_DEAL_FIELD_RECURRING_SINGLE_TYPE_DAY"] = "Tag(e)";
+$MESS["CRM_DEAL_FIELD_RECURRING_SINGLE_TYPE_WEEK"] = "Woche(n)";
+$MESS["CRM_DEAL_FIELD_RECURRING_SINGLE_TYPE_MONTH"] = "Monat(e)";
+$MESS["CRM_DEAL_FIELD_RECURRING_NOT_REPEAT"] = "Nicht wiederholen";
+$MESS["CRM_DEAL_FIELD_RECURRING_EVERYDAY"] = "Täglich";
+$MESS["CRM_DEAL_FIELD_RECURRING_EVERY_WEEK"] = "Wöchentlich";
+$MESS["CRM_DEAL_FIELD_RECURRING_EVERY_MONTH"] = "Monatlich";
+$MESS["CRM_DEAL_FIELD_RECURRING_EVERY_YEAR"] = "Jährlich";
+$MESS["CRM_DEAL_CREATION_PAGE_TITLE"] = "Neuer Auftrag";
+$MESS["CRM_DEAL_COPY_PAGE_TITLE"] = "Auftrag kopieren";
+$MESS["CRM_DEAL_FIELD_NUM_SIGN"] = "##DEAL_ID#";
+$MESS["CRM_DEAL_FIELD_CONTACT_LEGEND"] = "Mit dem Auftrag verknüpfte Kontakte";
+$MESS["CRM_DEAL_NOT_FOUND"] = "Auftrag wurde nicht gefunden.";
+$MESS["CRM_DEAL_FIELD_ID"] = "ID";
+$MESS["CRM_RECURRING_DEAL_B24_BLOCK_TITLE"] = "Wiederkehrende Aufträge sind in Tarifen \"Standard\" und \"Professional\" verfügbar";
+$MESS["CRM_RECURRING_DEAL_B24_BLOCK_TEXT"] = "Aufträge in Bitrix24 können automatisch erstellt werden. 
+Wiederkehrende Aufträge sparen Ihnen Zeit und Arbeitsaufwand. Ein Beispiel: Sie haben es vor, im Laufe des ganzen Jahres Standardverträge mit Ihren Kunden regelmäßig abzuschließen, bspw. wöchentlich. In dieser Situation ist ein wiederkehrender Auftrag genau die richtige Lösung für Sie.
+Erstellen Sie einen wiederkehrenden Auftrag, legen Sie einen Zeitplan und eine Pipeline fest. Neue Aufträge werden zum angegebenen Zeitpunkt automatisch erstellt, Sie werden dafür gar nichts mehr tun müssen!
+<ul class=\"hide-features-list\">
+<li class=\"hide-features-list-item\">Aufträge automatisch erstellen</li>
+<li class=\"hide-features-list-item\">Arbeitsleistung Ihrer Mitarbeiter erhöhen</li>
+<li class=\"hide-features-list-item\">Schneller Zugriff auf aktuelle wiederkehrende Aufträge- <a href=\"#LINK#\" target=\"_blank\" class=\"hide-features-more\">Mehr erfahren</a></li>
+</ul> 
+<strong>Wiederkehrende Aufträge sind in Tarifen \"Standard\" und \"Professional\" verfügbar</strong>
+";
+$MESS["CRM_DEAL_QUOTE_LINK"] = "Auftrag wurde aus dem Angebot <a target='_blank' href='#URL#'>#TITLE#</a> erstellt";
+$MESS["CRM_DEAL_FIELD_LOCATION_ID"] = "Standort";
+$MESS["CRM_DEAL_FIELD_UTM"] = "UTM-Parameter";
+$MESS["CRM_DEAL_RETURNING"] = "Wiederkehrender Auftrag";
+$MESS["CRM_DEAL_FIELD_SOURCE_ID"] = "Quelle";
+$MESS["CRM_DEAL_FIELD_SOURCE_DESCRIPTION"] = "Informationen zur Quelle";
+$MESS["CRM_DEAL_SOURCE_NOT_SELECTED"] = "Nicht ausgewählt";
+$MESS["CRM_DEAL_REPEATED_APPROACH"] = "Wiederholte Anfrage";
+?>
